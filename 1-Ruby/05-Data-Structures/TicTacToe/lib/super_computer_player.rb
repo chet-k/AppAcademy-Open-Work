606468f0b665f7ce470559ff49118fa0255a4a20
chet-k <chetkupchella@gmail.com>
2020-12-06 14:01:48 -0500

    add SuperComputerPlayer subclass that always wins or draws

--- 1-Ruby/05-Data-Structures/TicTacToe/lib/super_computer_player.rb
+++ 1-Ruby/05-Data-Structures/TicTacToe/lib/super_computer_player.rb
@@ -2,6 +2,20 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    move_tree = TicTacToeNode.new(game.board, mark)
+
+    #look for winning moves first
+    move_tree.children.each do |node|
+      return node.prev_move_pos if node.winning_node?(mark)
+    end
+
+    #no winning move found, go through tree again for non-loser
+    move_tree.children.each do |node|
+      return node.prev_move_pos if !node.losing_node?(mark)
+    end
+
+    #should not get through to here
+    raise
   end
 end
 
