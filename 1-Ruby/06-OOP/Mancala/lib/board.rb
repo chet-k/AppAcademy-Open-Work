61ec14fbedd0ce025b712f1296d5f60523209b41
chet-k <chetkupchella@gmail.com>
2020-12-06 19:31:24 -0500

    Start mancala HW with provided skeleton. Initialize Board class

---
+++ 1-Ruby/06-OOP/Mancala/lib/board.rb
@@ -0,0 +1,41 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @cups = Array.new(14)
+    place_stones
+  end
+
+  def place_stones
+    @cups.map!.with_index {|cup, i| cup = (i == 6 || i == 13) ? [] : [:stone, :stone, :stone, :stone]}
+  end
+
+  def valid_move?(start_pos)
+  end
+
+  def make_move(start_pos, current_player_name)
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+  end
+
+  def winner
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  b = Board.new("Chet","Hannah")
+  puts b.cups
+end
