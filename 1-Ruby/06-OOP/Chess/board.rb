8d52dff47bba914330183e258c81cead510f1f13
chet-k <chetkupchella@gmail.com>
2020-12-08 21:24:28 -0500

    add basic methods and placeholder code to support Board#move_piece

--- 1-Ruby/06-OOP/Chess/board.rb
+++ 1-Ruby/06-OOP/Chess/board.rb
@@ -1,6 +1,42 @@
-require_relative 'piece.rb'
+require_relative "piece.rb"
+require "byebug"
+
 class Board
     def initialize
-        @rows = Array.new(8) {Array.new(8)}
+        @null_piece = NullPiece.instance
+
+        @rows = Array.new(8) {Array.new(8) {@null_piece}}
+        populate_board
+    end
+
+    def [](pos)
+        r, c = pos
+        @rows[r][c]
+    end
+
+    def []=(pos, val)
+        r, c = pos
+        @rows[r][c] = val
+    end
+
+    def move_piece(color, start_pos, end_pos)
+        piece = self[start_pos]
+        self[end_pos] = piece
+        self[start_pos] = @null_piece
+    end
+
+    def valid_pos?(pos)
+    end
+
+    private
+
+    def populate_board
+        #black at top
+        (0..7).each {|column| @rows[0][column] = Piece.new }
+        (0..7).each {|column| @rows[1][column] = Piece.new }
+
+        #white at bottom
+        (0..7).each {|column| @rows[6][column] = Piece.new }
+        (0..7).each {|column| @rows[7][column] = Piece.new }
     end
 end
--- 1-Ruby/06-OOP/Chess/piece.rb
+++ 1-Ruby/06-OOP/Chess/piece.rb
@@ -1,4 +1,18 @@
+require "singleton"
+
 class Piece
-    def initialize(type = nil)
-    end
+        # def initialize(color, board, pos)
+        #     @color = color
+        #     @board = board
+        #     @pos = pos
+        # end
+end
+
+class NullPiece < Piece
+    include Singleton
+    attr_reader :color, :symbol
+    def initialize
+        @color = nil
+        @symbol = nil
+    end 
 end
