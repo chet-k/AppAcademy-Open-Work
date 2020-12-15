d52c096ac9391fabe0325a59697eb4cff4f102d6
chet-k <chetkupchella@gmail.com>
2020-12-15 10:31:23 -0600

    finish Simon game to specs

--- 1-Ruby/06-OOP/Simon/lib/simon.rb
+++ 1-Ruby/06-OOP/Simon/lib/simon.rb
@@ -1,8 +1,3 @@
-"""One important thing to note is that it is generally impractical to test 
-console user input, so the specs don't test require_sequence. The goal of 
-require_sequence is to prompt the user to repeat back the new sequence that 
-was just shown to them, one color at a time. If they give an incorrect color, 
-it should immediately set @game_over to be true."""
 require "byebug"
 
 class Simon
@@ -17,19 +12,47 @@ class Simon
   end
 
   def play
-
+    take_turn until @game_over
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    unless @game_over
+      round_success_message
+      @sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
+    @seq.each do |color|
+      system("clear")
+      puts color
+      sleep(0.5)
+    end
   end
 
   def require_sequence
+    @seq.each do |seq_color|
+      
+      system("clear")
+      puts "Your turn! Type each color."
+      print "> "
 
+      recalled_color = gets.chomp.downcase
+      
+      unless recalled_color == seq_color
+        puts "!! WRONG !! "
+        puts "you guessed #{recalled_color}."
+        puts "The correct color was #{seq_color}."
+        
+        @game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
@@ -37,20 +60,23 @@ class Simon
   end
 
   def round_success_message
-
+    system("clear")
+    puts "Nice one! Simon says......"
+    sleep(0.5)
   end
 
   def game_over_message
-
+    puts "GAME OVER."
   end
 
   def reset_game
-
+    @game_over = false
+    @seq = []
+    @sequence_length = 1
   end
 end
 
 if $PROGRAM_NAME == __FILE__
   s = Simon.new
-  s.add_random_color
-
+  s.play
 end
