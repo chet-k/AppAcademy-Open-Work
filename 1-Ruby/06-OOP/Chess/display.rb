1ca1f920ff703d59577736f75ddc107935dd7254
chet-k <chetkupchella@gmail.com>
2020-12-16 11:27:21 -0600

    alternate square background colors in Display#render

--- 1-Ruby/06-OOP/Chess/display.rb
+++ 1-Ruby/06-OOP/Chess/display.rb
@@ -15,12 +15,26 @@ class Display
         @board.rows.each_with_index do |row, m|
             row_to_render = []
             row.each_with_index do |piece, n|
-                bg = [m,n] == @cursor.cursor_pos ? :light_white : :light_black
+                default_bg = color_by_square([m,n])
+                bg = [m,n] == @cursor.cursor_pos ? :light_blue : default_bg
                 row_to_render << piece.to_s.colorize(:background => bg)
             end
+        
             puts row_to_render.join(" | ")
             puts horiz_divider
         end
+        
         nil
+    end
+
+    def color_by_square(pos)
+        m, n = pos
+        light_bg = :light_white
+        dark_bg = :light_black
+        if m.even?
+            n.even? ? light_bg : dark_bg
+        else
+            n.even? ? dark_bg : light_bg
+        end
     end
 end
