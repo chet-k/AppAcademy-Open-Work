d4a71b6d4f1d5c8eb87f50095be8ed4a7e75293d
chet-k <chetkupchella@gmail.com>
2020-12-19 16:31:56 -0600

    complete RSpec HW

--- 1-Ruby/07-RSPEC-TDD/RSpecHW/spec/dessert_spec.rb
+++ 1-Ruby/07-RSPEC-TDD/RSpecHW/spec/dessert_spec.rb
@@ -6,8 +6,8 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  subject(:brownie) { Dessert.new("brownie", 10, chef) }
   let(:chef) { double("chef") }
+  subject(:brownie) { Dessert.new("brownie", 10, chef) }
 
   describe "#initialize" do
     it "sets a type" do 
@@ -28,24 +28,46 @@ describe Dessert do
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      expect(brownie.ingredients).to_not include("sugar")
+      brownie.add_ingredient("sugar")
+      expect(brownie.ingredients).to include("sugar")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      brownie.add_ingredient("sugar")
+      brownie.add_ingredient("chocolate")
+      brownie.mix!
+      expect(brownie.ingredients.length).to eq(2)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      brownie.eat(2)
+      expect(brownie.quantity).to eq(8)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect{brownie.eat(12)}.to raise_error("not enough left!")
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize)
+      brownie.serve
+      expect(chef).to have_received(:titleize)      
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      allow(chef).to receive(:bake)
+      brownie.make_more
+      expect(chef).to have_received(:bake).with(brownie)
+    end
   end
 end
