16a191ca0dc08ea0ac41f258dc35d05191141baa
chet-k <chetkupchella@gmail.com>
2020-12-20 16:49:53 -0600

    add Array#my_transpose method and write test first

--- 1-Ruby/07-RSPEC-TDD/TDD-projects/lib/tdd.rb
+++ 1-Ruby/07-RSPEC-TDD/TDD-projects/lib/tdd.rb
@@ -5,12 +5,30 @@ class Array
 
     def two_sum
         out = []
-        
+
         element_indices = Hash.new { |h, k| h[k] = [] }
         self.each_with_index { |el, i| element_indices[-el] << i }
 
         self.each_with_index do |el, i1| 
             element_indices[el].each {|i2| out << [i1, i2] if i1 < i2}
+        end
+
+        out
+    end
+
+    def my_transpose
+        if !self[0].is_a? Array
+            raise TypeError.new("2D Array required")
+        elsif self.any? {|subarr| subarr.length != self[0].length}
+            raise IndexError.new("subarray lengths do not match")
+        end
+
+        out =  Array.new(self[0].length) {Array.new(self.length)}
+
+        (0...self.length).each do |m|
+            (0...self[0].length).each do |n|
+                out[n][m] = self[m][n]
+            end
         end
 
         out
--- 1-Ruby/07-RSPEC-TDD/TDD-projects/spec/tdd_spec.rb
+++ 1-Ruby/07-RSPEC-TDD/TDD-projects/spec/tdd_spec.rb
@@ -23,4 +23,18 @@ describe Array do
             expect(pairs[0][0] < pairs[0][1]).to be true
         end
     end
+
+    context "#my_transpose" do 
+        it "transposes rows to columns" do 
+            expect([[0, 1, 2],[3, 4, 5],[6, 7, 8]].my_transpose).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]]) 
+        end
+
+        it "raises a TypeError for 1D arrays" do 
+            expect{[0,1,2,3].my_transpose}.to raise_error(TypeError)
+        end
+
+        it "raises an IndexError if the length of subarrays don't match" do 
+            expect {[[0, 1, 2],[3, 4, 5, 6]].my_transpose}.to raise_error(IndexError)
+        end
+    end
 end
