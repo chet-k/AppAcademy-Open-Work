9fdf8e56db080ceccb994749942d5cea6afac964
chet-k <chetkupchella@gmail.com>
2020-12-20 16:14:07 -0600

    add Array#two_sum test and implement method

--- 1-Ruby/07-RSPEC-TDD/TDD-projects/lib/tdd.rb
+++ 1-Ruby/07-RSPEC-TDD/TDD-projects/lib/tdd.rb
@@ -2,4 +2,17 @@ class Array
     def my_uniq
         Set.new(self).to_a
     end
+
+    def two_sum
+        out = []
+        
+        element_indices = Hash.new { |h, k| h[k] = [] }
+        self.each_with_index { |el, i| element_indices[-el] << i }
+
+        self.each_with_index do |el, i1| 
+            element_indices[el].each {|i2| out << [i1, i2] if i1 < i2}
+        end
+
+        out
+    end
 end
--- 1-Ruby/07-RSPEC-TDD/TDD-projects/spec/tdd_spec.rb
+++ 1-Ruby/07-RSPEC-TDD/TDD-projects/spec/tdd_spec.rb
@@ -2,15 +2,25 @@ require 'rspec'
 require 'tdd'
 
 describe Array do
-    describe
-    subject(:arr) {[6,6,7,12,3,7,7,4,4,5,1,2,3,4,3,4,5,1]}
     context "#my_uniq" do
+        subject(:arr) {[6,6,7,12,3,7,7,4,4,5,1,2,3,4,3,4,5,1] }
         let (:uniques) { arr.my_uniq }
         it "returns unique elements" do
             expect(uniques.all? {|el| uniques.count(el) == 1}).to be true
         end
         it "returns elements in the order they appear"  do
             expect(arr.index(uniques[0]) < arr.index(uniques[1])).to be true
+        end
+    end
+
+    context "#two_sum" do
+        subject(:arr) { [-1, 0, 1, 2, -2, 1] }
+        let(:pairs) { arr.two_sum }
+        it "returns all pairs of indices whose elements sum to zero" do
+            expect(pairs).to eq([[0, 2], [0, 5], [3, 4]])
+        end
+        it "pairs are sorted dictionary-wise" do
+            expect(pairs[0][0] < pairs[0][1]).to be true
         end
     end
 end
