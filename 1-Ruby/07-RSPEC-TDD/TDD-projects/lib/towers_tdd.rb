9b3e294b35d0ba9956ad2521e12af3022ddab295
chet-k <chetkupchella@gmail.com>
2020-12-21 08:56:02 -0600

    add #initialize and #move tests to Towers specs

--- 1-Ruby/07-RSPEC-TDD/TDD-projects/lib/towers_tdd.rb
+++ 1-Ruby/07-RSPEC-TDD/TDD-projects/lib/towers_tdd.rb
@@ -1,2 +1,31 @@
 class Towers_Game
+    attr_reader :size, :towers
+    def initialize(size = 3)
+        @size = [size, 3].max
+        @towers = [(1..@size).to_a.reverse,[],[]]
+    end
+
+    def [](n)
+        @towers[n]
+    end
+
+    def valid_move?(start, finish)
+        #start has disks
+        #finish tower is empty
+        #top disk is larger than top disk on finish
+    end
+
+    def move(start, finish)
+
+    end
+
+    def won?
+    end
+
+    def get_input
+    end
+
+    private
+    def valid_input
+    end
 end
--- 1-Ruby/07-RSPEC-TDD/TDD-projects/spec/towers_tdd_spec.rb
+++ 1-Ruby/07-RSPEC-TDD/TDD-projects/spec/towers_tdd_spec.rb
@@ -2,11 +2,51 @@ require 'rspec'
 require 'towers_tdd'
 
 describe Towers_Game do
+    let(:game_size) {5}
+    subject(:game) {Towers_Game.new(game_size)}
+    
+    describe "#initialize" do 
+        it "creates 3 towers" do 
+            expect(game.towers.length).to eq(3)
+        end
+
+        it "sets game size by input" do
+            expect(game.size).to eq(game_size)
+        end
+
+        it "stacks all disks on the first tower, equal to game_size" do 
+            expect(game[0].length).to eq(game_size)
+            expect(game[1].length).to eq(0)
+            expect(game[2].length).to eq(0)
+        end
+
+        it "initializes with minimum size of 3" do
+            small_game = Towers_Game.new(2)
+            expect(small_game[0].length).to eq(3)
+        end
+    end
+    
     describe "#move" do
-        it "removes the top disk from the first pile"
-        it "adds that disk to the top of the second pile"
+        before(:example) {game.move(0,1)}
+        it "removes the top disk from the first pile" do     
+            expect(game[0].length).to eq(game_size-1)
+        end
+        it "adds that disk to the top of the second pile" do
+            expect(game[1].length).to eq(1)
+        end
     end
 
+    describe "#valid_move?" do
+        before(:example) do 
+            game.move(0,1)
+            game.move(0,2)
+            game.move(1,2)
+        end
+        it "returns false if start has no disks"
+        it "returns true if start has disks and finish is empty"
+        it "returns true if start has disks and and top disk is larger than top disk on finish"
+    end
+    
     describe "#won?" do
         it "returns true only when all disks have been moved to the third tower"
     end
