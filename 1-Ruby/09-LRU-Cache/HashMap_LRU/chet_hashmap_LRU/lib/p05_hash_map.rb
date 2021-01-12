720b0f10e11a6b1c415c00474d8dfbedb8f66c8d
chet-k <chetkupchella@gmail.com>
2021-01-12 17:37:50 -0500

    Finish part 5, custom hash map

--- 1-Ruby/09-LRU-Cache/HashMap_LRU/chet_hashmap_LRU/lib/p05_hash_map.rb
+++ 1-Ruby/09-LRU-Cache/HashMap_LRU/chet_hashmap_LRU/lib/p05_hash_map.rb
@@ -1,6 +1,7 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -9,27 +10,44 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    if include?(key)
+      bucket(key).update(key, val)
+    else
+      @count += 1
+      resize! if @count >= num_buckets
+      bucket(key).append(key, val)
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    @count -= 1 if include?(key)
+    bucket(key).remove(key)
   end
 
-  def each
+  def each(&prc)
+    prc ||= Proc.new {|el| el}
+    @store.each do |b|
+      b.each do |node|
+        prc.call([node.key, node.val])
+      end
+    end
+    self
   end
 
-  # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -41,9 +59,19 @@ class HashMap
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { LinkedList.new }
+    
+    @store.each do |b| 
+      b.each do |node|
+        k, v = node.key, node.val
+        new_store[k.hash % (num_buckets * 2)].append(k, v)
+      end
+    end
+
+    @store = new_store
   end
 
-  def bucket(key)
-    # optional but useful; return the bucket corresponding to `key`
+  def bucket(key) 
+    @store[key.hash % num_buckets]
   end
 end
