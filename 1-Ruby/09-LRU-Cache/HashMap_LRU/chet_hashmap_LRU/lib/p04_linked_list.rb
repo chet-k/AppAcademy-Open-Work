0f5cad3db8ec1ec6b558c83f72820c7c1176e5bd
chet-k <chetkupchella@gmail.com>
2021-01-12 17:05:14 -0500

    Finish part 4, custom doubly linked list

--- 1-Ruby/09-LRU-Cache/HashMap_LRU/chet_hashmap_LRU/lib/p04_linked_list.rb
+++ 1-Ruby/09-LRU-Cache/HashMap_LRU/chet_hashmap_LRU/lib/p04_linked_list.rb
@@ -14,49 +14,85 @@ class Node
   end
 
   def remove
-    # optional but useful, connects previous link to next link
-    # and removes self from list.
+    @prev.next = @next
+    @next.prev = @prev
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next, @tail.prev = @tail, @head
   end
 
   def [](i)
-    each_with_index { |link, j| return link if i == j }
+    each_with_index { |node, j| return node if i == j }
     nil
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each {|node| return node.val if node.key == key}
+    nil
   end
 
   def include?(key)
+    any? {|node| node.key == key}
   end
 
   def append(key, val)
+    unless include?(key)
+      new_node = Node.new(key, val)
+      new_node.prev, new_node.next = last, @tail
+      last.next, @tail.prev = new_node, new_node
+    end
   end
 
   def update(key, val)
+    each {|node| node.val = val if node.key == key}
   end
 
   def remove(key)
+    each {|node| node.remove if node.key == key}
   end
 
-  def each
+  def each(&prc)
+    prc ||= Proc.new {|el| el}
+    node = first
+    until node == @tail
+      prc.call(node)
+      node = node.next
+    end
+    self
+  end
+  
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  h = { first: 1, second: 2, third: 3 }
+  
+  list = LinkedList.new
+  h.each do |key, val|
+    list.append(key, val)
   end
 
-  # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  puts list.to_s
 end
