9a0a664122b7fd1f7d4262e3ead9884f480dccc7
chet-k <chetkupchella@gmail.com>
2021-01-12 15:33:17 -0500

    create custom hashes, phase 2

--- 1-Ruby/09-LRU-Cache/HashMap_LRU/chet_hashmap_LRU/lib/p02_hashing.rb
+++ 1-Ruby/09-LRU-Cache/HashMap_LRU/chet_hashmap_LRU/lib/p02_hashing.rb
@@ -4,18 +4,29 @@ end
 
 class Array
   def hash
+    int_out = 1
+    self.each_with_index do |el, i|
+      int_out = int_out ^ (el.hash + i)
+    end
+    int_out
   end
 end
 
 class String
   def hash
+    int_out = 1
+    self.each_char.with_index do |c, i|
+      int_out = int_out ^ (c.ord.hash + i)
+    end
+    int_out
   end
 end
 
 class Hash
-  # This returns 0 because rspec will break if it returns nil
-  # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    keys_int, vals_int = 1, 1
+    self.keys.each   {|k| keys_int = keys_int ^ k.hash}
+    self.values.each {|v| vals_int = vals_int ^ v.hash}
+    vals_int ^ keys_int
   end
 end
