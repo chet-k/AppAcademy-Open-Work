8cbb114914f97b6b852cf18cea3dd1753bda84f3
chet-k <chetkupchella@gmail.com>
2021-01-12 19:04:15 -0500

    Finish part 6 custom LRU cache

--- 1-Ruby/09-LRU-Cache/HashMap_LRU/chet_hashmap_LRU/lib/p06_lru_cache.rb
+++ 1-Ruby/09-LRU-Cache/HashMap_LRU/chet_hashmap_LRU/lib/p06_lru_cache.rb
@@ -1,6 +1,8 @@
 require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
 
+# linked list: FIRST is oldest, LAST is youngest
+
 class LRUCache
   def initialize(max, prc)
     @map = HashMap.new
@@ -14,6 +16,15 @@ class LRUCache
   end
 
   def get(key)
+    unless @map[key]
+      calc!(key) 
+      eject! if count > @max
+    end
+
+    node = @map[key]
+    update_node!(node)
+
+    node.val
   end
 
   def to_s
@@ -22,14 +33,21 @@ class LRUCache
 
   private
 
-  def calc!(key)
-    # suggested helper method; insert an (un-cached) key
+  def calc!(key) # suggested helper method; insert an (un-cached) key
+    val = @prc.call(key)
+    @store.append(key, val)
+    @map[key] = @store.last
   end
 
-  def update_node!(node)
-    # suggested helper method; move a node to the end of the list
+  def update_node!(node) # suggested helper method; move a node to the end of the list
+    node.remove
+    @store.append(node.key, node.val)
+    @map[node.key] = @store.last
   end
 
   def eject!
+    node = @store.first
+    @map.delete(node.key)
+    node.remove
   end
 end
