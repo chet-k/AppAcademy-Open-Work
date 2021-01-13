722b174eb33bc462ca2b1a6d6da59eb8a0634f9f
chet-k <chetkupchella@gmail.com>
2021-01-12 21:51:00 -0500

    finish bonus dynamic array implementation

--- 1-Ruby/09-LRU-Cache/HashMap_LRU/chet_hashmap_LRU/lib/p07_dynamic_array_bonus.rb
+++ 1-Ruby/09-LRU-Cache/HashMap_LRU/chet_hashmap_LRU/lib/p07_dynamic_array_bonus.rb
@@ -27,6 +27,7 @@ class StaticArray
 end
 
 class DynamicArray
+  include Enumerable
   attr_accessor :count
 
   def initialize(capacity = 8)
@@ -35,9 +36,21 @@ class DynamicArray
   end
 
   def [](i)
+    return nil if i >= @count || -i > @count
+    i = i < 0 ? @count + i : i
+
+    @store[i]
   end
 
   def []=(i, val)
+    if i < 0
+      return nil if -i > @count
+      i = @count + i
+    else
+      push(nil) until @count >= i + 1
+    end
+
+    @store[i] = val
   end
 
   def capacity
@@ -45,27 +58,56 @@ class DynamicArray
   end
 
   def include?(val)
+    @count.times {|i| return true if @store[i] == val}
+    false
   end
 
   def push(val)
+    @count += 1
+    resize! if count > capacity
+    
+    @store[@count - 1] = val
   end
 
   def unshift(val)
+    return nil if @count == 0
+
+    @count += 1
+    resize! if count > capacity
+
+    (1..@count-1).reverse_each { |i| @store[i] = @store[i-1] }
+    self[0] = val
   end
 
   def pop
+    return nil if @count == 0
+    out = last
+    @count -= 1
+    out
   end
 
   def shift
+    return nil if @count == 0
+    out = first
+    
+    @count -= 1
+    @count.times {|i| @store[i] = @store[i+1]}
+    
+    out
   end
 
   def first
+    @store[0]
   end
 
   def last
+    @store[@count - 1]
   end
 
-  def each
+  def each(&prc)
+    prc ||= Proc.new {|el| el}
+    @count.times {|i| prc.call(@store[i])}
+    self
   end
 
   def to_s
@@ -74,7 +116,9 @@ class DynamicArray
 
   def ==(other)
     return false unless [Array, DynamicArray].include?(other.class)
-    # ...
+    return false unless other.length == @count
+    @count.times {|i| return false unless other[i] == self[i] }
+    true
   end
 
   alias_method :<<, :push
@@ -83,5 +127,8 @@ class DynamicArray
   private
 
   def resize!
+    new_store = StaticArray.new(@store.length * 2)
+    capacity.times {|i| new_store[i] = @store[i]}
+    @store = new_store
   end
 end
