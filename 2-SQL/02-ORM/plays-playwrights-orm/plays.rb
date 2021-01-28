ca2972584f2b798a533fab8ff7c492c871c8a49a
chet-k <chetkupchella@gmail.com>
2021-01-27 20:22:47 -0500

    finish Playwright ORM

--- 2-SQL/02-ORM/plays-playwrights-orm/plays.rb
+++ 2-SQL/02-ORM/plays-playwrights-orm/plays.rb
@@ -36,6 +36,7 @@ class Play
 
   def self.find_by_playwright(find_name)
     playwright = Playwright.find_by_name(find_name)
+    raise "#{find_name} not found in DB" unless playwright
     
     data = PlayDBConnection.instance.execute(<<-SQL, playwright.id)
       SELECT
@@ -46,7 +47,7 @@ class Play
         playwright_id = ?
     SQL
 
-    plays.map { |play| Play.new(play)}
+    data.map { |play| Play.new(play)}
   end
   
   def initialize(options)
@@ -77,5 +78,72 @@ class Play
       WHERE
         id = ?
     SQL
+  end
+end
+
+class Playwright
+  attr_accessor :id, :name, :birth_year
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Play.new(datum) }
+  end
+
+  def self.find_by_name(find_name)
+    data = PlayDBConnection.instance.execute(<<-SQL, find_name)
+      SELECT
+        *
+      FROM
+        playwrights
+      WHERE
+        name = ?
+    SQL
+    return nil unless data.length > 0
+
+    Playwright.new(data[0])
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        plays (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      UPDATE
+        plays
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    raise "#{self} not in database" unless self.id
+    data = PlayDBConnection.instance.execute(<<-SQL)
+      SELECT
+        *
+      FROM
+        plays
+      WHERE
+        playwright_id = ?
+    SQL
+    return nil unless data.length > 0
+
+    data.map { |play| Play.new(play)}
   end
 end
