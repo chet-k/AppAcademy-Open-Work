28dcf8c15d59d0ca156d2162c66d1c6f1753e874
chet-k <chetkupchella@gmail.com>
2021-02-20 17:05:45 -0500

    finish intro rails hw associations

--- 2-SQL/03-Active-Record/intro_to_rails/intro_project/app/models/house.rb
+++ 2-SQL/03-Active-Record/intro_to_rails/intro_project/app/models/house.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: houses
+#
+#  id      :bigint           not null, primary key
+#  address :string           not null
+#
 class House < ApplicationRecord
-    
+    has_many(:people, {
+        primary_key: :id,
+        foreign_key: :house_id,
+        class_name: :Person
+    })
 end
--- 2-SQL/03-Active-Record/intro_to_rails/intro_project/app/models/person.rb
+++ 2-SQL/03-Active-Record/intro_to_rails/intro_project/app/models/person.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id       :bigint           not null, primary key
+#  name     :string           not null
+#  house_id :integer
+#
 class Person < ApplicationRecord
-    
+    belongs_to(:house, {
+        primary_key: :id,
+        foreign_key: :house_id,
+        class_name: :House
+    })
 end
