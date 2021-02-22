5041f4b8c0a3938ac1eb4ae9e85dae4d6a27ebe3
chet-k <chetkupchella@gmail.com>
2021-02-21 21:58:21 -0500

    create associations between user and shortenedurl

--- 2-SQL/03-Active-Record/URL_shortener/URLShortener/app/models/shortened_url.rb
+++ 2-SQL/03-Active-Record/URL_shortener/URLShortener/app/models/shortened_url.rb
@@ -1,4 +1,29 @@
-class ShortenedUrl
+require 'securerandom'
+
+class ShortenedUrl < ApplicationRecord
     validates :long_url, presence: true
     validates :short_url, uniqueness: true
+
+    belongs_to :submitter,
+        class_name: 'User',
+        foreign_key: :user_id,
+        primary_key: :id 
+
+    def self.create_short_url_for_user!(user, long_url)
+        ShortenedUrl.create!(
+            user_id: user.id, 
+            long_url: long_url, 
+            short_url: ShortenedUrl.random_code
+        )
+    end
+
+    def self.random_code
+        out = SecureRandom.urlsafe_base64
+        
+        while ShortenedUrl.exists?(:short_url => out)
+            out = SecureRandom.urlsafe_base64
+        end
+        
+        out
+    end
 end
