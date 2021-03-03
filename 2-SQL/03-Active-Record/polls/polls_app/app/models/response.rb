40953721b2dfd25df46fcbad2c1fa8a4e9e328ee
chet-k <chetkupchella@gmail.com>
2021-03-02 21:11:53 -0500

    add not_duplicate_response validation

--- 2-SQL/03-Active-Record/polls/polls_app/app/models/response.rb
+++ 2-SQL/03-Active-Record/polls/polls_app/app/models/response.rb
@@ -1,5 +1,6 @@
 class Response < ApplicationRecord
     validates :user_id, :answer_choice_id, presence: true
+    validates :not_duplicate_response
 
     belongs_to :answer_choice,
         class_name: :AnswerChoice,
@@ -10,4 +11,28 @@ class Response < ApplicationRecord
         class_name: :User,
         foreign_key: :user_id,
         primary_key: :id
+    
+    has_one :question,
+        through: :answer_choice,
+        source: :question
+
+    def sibling_responses
+        self
+            .question
+            .responses
+            .where.not(id: self.id)
+    end
+
+    def respondent_already_answered?
+        self
+            .sibling_responses
+            .exists?(user_id: respondent.id)
+    end
+
+    private
+    def not_duplicate_response
+        if respondent_already_answered?
+            errors[:base] << 'Invalid response - user already answered!'
+        end
+    end
 end
