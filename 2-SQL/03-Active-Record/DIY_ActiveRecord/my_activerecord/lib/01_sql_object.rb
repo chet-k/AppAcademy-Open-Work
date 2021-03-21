76de074486d4a0f3af3a7f6db319efde84b18b04
chet-k <chetkupchella@gmail.com>
2021-03-21 13:41:59 -0400

    phase 1 a-f

--- 2-SQL/03-Active-Record/DIY_ActiveRecord/my_activerecord/lib/01_sql_object.rb
+++ 2-SQL/03-Active-Record/DIY_ActiveRecord/my_activerecord/lib/01_sql_object.rb
@@ -5,26 +5,48 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        "#{self.table_name}"
+      LIMIT
+        0
+    SQL
+    @columns = cols.map!(&:to_sym)
   end
 
   def self.finalize!
+    self.columns.each do |name|
+      define_method(name)  { self.attributes[name] }
+      define_method("#{name}=") { |value| self.attributes[name] = value }
+    end
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    @table_name || self.to_s.tableize
   end
 
   def self.all
-    # ...
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        #{self.table_name}.*
+      FROM
+        #{self.table_name}
+    SQL
+
+    puts results
+
+    self.parse_all(results)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |params| self.new(params) }
   end
 
   def self.find(id)
@@ -32,15 +54,22 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |attr_name, value|
+      attr_name = attr_name.to_sym
+      if self.class.columns.include?(attr_name)
+        self.send("#{attr_name}=",value)
+      else
+        raise "unknown attribute '#{attr_name}'" 
+      end
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {} 
   end
 
   def attribute_values
-    # ...
+    
   end
 
   def insert
