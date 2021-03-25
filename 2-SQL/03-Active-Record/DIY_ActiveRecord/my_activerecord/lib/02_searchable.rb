d00667a6b98e40e6702817cfc4cc306b53c1c7e9
chet-k <chetkupchella@gmail.com>
2021-03-24 20:15:24 -0400

    phase 2 searchable

--- 2-SQL/03-Active-Record/DIY_ActiveRecord/my_activerecord/lib/02_searchable.rb
+++ 2-SQL/03-Active-Record/DIY_ActiveRecord/my_activerecord/lib/02_searchable.rb
@@ -3,10 +3,10 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map {|key| "#{key} = ?"}.join(" AND ")
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
