4b5c1bb60c17cfdc2847a326ffc5f00ea79b91c4
chet-k <chetkupchella@gmail.com>
2021-06-22 21:56:35 -0400

    explore postman requests and create User model

--- 3-Rails/first-routes-controllers/first_routes_controllers/app/controllers/users_controller.rb
+++ 3-Rails/first-routes-controllers/first_routes_controllers/app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    def index
+        render json: params
+        #render plain: "I'm in the index action!"
+    end
+
+    def create
+        render json: params
+    end
+
+    def show
+        render json: params
+    end
 end
