008b1057e7ab2c45acc5d1a97b2bc8421536c745
chet-k <chetkupchella@gmail.com>
2021-06-23 21:44:28 -0400

    add crud methods to users_controller.rb

--- 3-Rails/first-routes-controllers/first_routes_controllers/app/controllers/users_controller.rb
+++ 3-Rails/first-routes-controllers/first_routes_controllers/app/controllers/users_controller.rb
@@ -1,14 +1,38 @@
 class UsersController < ApplicationController
     def index
-        render json: params
-        #render plain: "I'm in the index action!"
+        render json: User.all
+        
     end
 
     def create
-        render json: params
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-        render json: params
+        render json: User.find(params[:id])
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        render json: user
+    end
+
+    def update
+        user = User.find(params[:id])
+        if user.update(user_params) #update_attributes is deprecated
+            render json: user
+        else
+            render json: user.errors, status: :unprocessable_entity
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 end
