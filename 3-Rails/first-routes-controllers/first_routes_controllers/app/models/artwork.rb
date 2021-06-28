c6ab0db88bd1279c8b825a3e8d16bff364164e27
chet-k <chetkupchella@gmail.com>
2021-06-27 21:22:29 -0400

    add artworks table and indices to database

---
+++ 3-Rails/first-routes-controllers/first_routes_controllers/app/models/artwork.rb
@@ -0,0 +1,8 @@
+class Artwork < ApplicationRecord
+    validates :title, presence: true
+    validates :image_url, presence: true
+    validates :artist_id, presence: true
+
+    validates :image_url, uniqueness: true
+    validates :title, uniqueness: {scope: :artist_id}
+end
